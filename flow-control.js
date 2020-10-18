d97eedee073dc4386905839c3fd6f113efe322bf
James Donovan <jameswilliamdonovan@gmail.com>
2020-10-18 14:57:29 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -30,7 +30,7 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch (age) {
-    case (10 + 5):
+    case (age === 15):
       return 'You are a teenager';
       break;
     default:
