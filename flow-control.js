5dd8cd83ad4782918dd6eab95e919255d4bdbe8c
James Donovan <jameswilliamdonovan@gmail.com>
2020-10-18 14:44:59 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,5 +1,5 @@
 function basicTeenager(age) {
-  if (age > 12 && age < 20) {
+  if (age >= 13 && age <= 19) {
     return 'You are a teenager!';
   } else {
     return undefined;
@@ -7,11 +7,21 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-
+  if (age >= 13 && age <= 19) {
+    return 'You are a teenager!';
+  } else {
+    return 'You are not a teenager';
+  }
 }
 
 function ageChecker(age) {
-
+  if (age < 13) {
+    return 'You are a kid';
+  } else if (age <= 20) {
+    return 'You are a grownup';
+  } else {
+    return 'You are a teenager';
+  }
 }
 
 function ternaryTeenager(age) {
